0b1fdfa62731a9407c950de093d19d2151d59b12
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-07-06 06:41:48 +0200

    path to *.h corrected

--- has_composite_shapes.cpp
+++ has_composite_shapes.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <libdjvu/DjVuDocument.h>
-#include <libdjvu/JB2Image.h>
+#include <DjVuDocument.h>
+#include <JB2Image.h>
 
 int main(int argc, char **argv) {
     if (argc < 2) {
