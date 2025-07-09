a6fd71ee023207d7c4a9bb8b38321b6f075d87f5
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-07-09 10:32:19 +0200

    restored

--- djvuimage_utils.h
+++ djvuimage_utils.h
@@ -1,8 +1,6 @@
 #ifndef DJVUIMAGE_UTILS_H
 #define DJVUIMAGE_UTILS_H
 
-#include <cstdint>  // ensures uint16_t and uint32_t are known before DjVu headers
-
 #include <libdjvu/ddjvuapi.h>
 #include "external/DjVuImage.h"
 
