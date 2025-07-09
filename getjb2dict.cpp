c2186926b1e28d009a78d5eb3040c39b9a583d53
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-07-09 11:16:34 +0200

    previous fix restored

--- getjb2dict.cpp
+++ getjb2dict.cpp
@@ -6,8 +6,7 @@
 // as published by the Free Software Foundation; either version 2 of the License,
 // or (at your option) any later version.
 
-#include <cstdint>  // ensures uint16_t and uint32_t are known before DjVu headers
-
+#include "fix_stdint.h"
 #include <libdjvu/ddjvuapi.h>
 #include "external/JB2Image.h"
 #include "djvuimage_utils.h"
